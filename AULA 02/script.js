30ee8c50bbb97329d3e1efaff020ee55768cb3de
Carol Malveira <carolmalveiraa12@icloud.com>
2024-08-26 19:19:00 -0300

    script aula 02

--- AULA 02/script.js
+++ AULA 02/script.js
@@ -76,22 +76,20 @@
 
 // Dada uma matriz valores com 2 linhas e 4 colunas, encontre o menor valor presente na matriz e armazene-o na variável menorValor. Utilize o índice para acessar os valores da matriz e realizar as comparações com Math.min.
 
-// const valores = [
-//   [15, 12, 18, 10],
-//   [14, 9, 16, 11],
-// ];
+const valores = [
+  [15, 12, 18, 10],
+  [14, 9, 16, 11],
+];
 
-// let menorValor = valores[0][0];
+let menorValor = valores[0][0];
 
-// for (let i = 0; i < valores.length; i++) {
-//     for (let j = 0; j < valores[i].length; j++) {
-//         if (valores[i][j] < menorValor) {
-//             menorValor = valores[i][j];
-//         }
-//     }
-// }
+for (let i = 0; i < valores.length; i++) {
+  for (let j = 0; j < valores[i].length; j++) {
+    if (valores[i][j] < menorValor) {
+      menorValor = valores[i][j];
+    }
+  }
+}
 
-// console.log(menorValor);
-
-// menorValor = Math.min(...valores.flat());
-// console.log(menorValor);
+menorValor = Math.min(...valores.flat());
+console.log(menorValor);
