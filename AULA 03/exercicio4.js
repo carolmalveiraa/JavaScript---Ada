1b13ebd03c67f3b2d27feb23b9ea6411fcabe389
Carol Malveira <carolmalveiraa12@icloud.com>
2024-08-28 19:44:30 -0300

    correção exercicio 4

--- AULA 03/exercicio4.js
+++ AULA 03/exercicio4.js
@@ -1,19 +1,10 @@
-// ### 4. Buscando a palavra mais repitida no texto.
+let texto = `Ut interdum tellus sit amet arcu viverra facilisis. Mauris diam magna, condimentum ac justo nec, rhoncus iaculis odio. Sed tincidunt lacus ut risus laoreet sagittis. Curabitur tristique egestas libero, nec vulputate ligula pellentesque auctor. Mauris vitae est lacinia, condimentum felis eget, consequat eros. Proin tempor ligula felis, sit amet malesuada purus viverra sit amet. Nunc lobortis porta est a mattis.
 
-// Cria um algoritmo que diz qual a palavra mais repetida dentro da seguinte texto:
+Curabitur congue tincidunt velit eu placerat. Aliquam sed urna pretium, laoreet ante nec, ultrices eros. Sed ullamcorper elit id dolor congue fringilla. Aenean fringilla sed risus ac consequat. Mauris turpis leo, volutpat eget varius sit amet, fermentum id lectus. Nunc quis gravida neque. Nulla eleifend risus diam, ut dictum mauris lobortis ut. Donec vitae nisi ullamcorper turpis accumsan malesuada a id mauris.
 
-// Ut interdum tellus sit amet arcu viverra facilisis. Mauris diam magna, condimentum ac justo nec, rhoncus iaculis odio. Sed tincidunt lacus ut risus laoreet sagittis. Curabitur tristique egestas libero, nec vulputate ligula pellentesque auctor. Mauris vitae est lacinia, condimentum felis eget, consequat eros. Proin tempor ligula felis, sit amet malesuada purus viverra sit amet. Nunc lobortis porta est a mattis.
+Vestibulum aliquet tellus id purus consequat, eget vehicula erat porttitor. In hac habitasse platea dictumst. Nunc semper faucibus enim sit amet tristique. Quisque egestas, massa luctus laoreet porttitor, eros lorem semper enim, sed mattis turpis justo ut est. Proin ac elit tincidunt, lacinia est nec, ultrices purus. Quisque sem dui, commodo a dui consectetur, posuere accumsan orci. Maecenas luctus, nibh ac molestie ornare, mi odio placerat metus, vitae ornare lorem lectus a erat. Maecenas imperdiet, dui nec tincidunt sollicitudin, orci velit efficitur tortor, id interdum odio nibh nec sapien.`;
 
-// Curabitur congue tincidunt velit eu placerat. Aliquam sed urna pretium, laoreet ante nec, ultrices eros. Sed ullamcorper elit id dolor congue fringilla. Aenean fringilla sed risus ac consequat. Mauris turpis leo, volutpat eget varius sit amet, fermentum id lectus. Nunc quis gravida neque. Nulla eleifend risus diam, ut dictum mauris lobortis ut. Donec vitae nisi ullamcorper turpis accumsan malesuada a id mauris.
-
-// Vestibulum aliquet tellus id purus consequat, eget vehicula erat porttitor. In hac habitasse platea dictumst. Nunc semper faucibus enim sit amet tristique. Quisque egestas, massa luctus laoreet porttitor, eros lorem semper enim, sed mattis turpis justo ut est. Proin ac elit tincidunt, lacinia est nec, ultrices purus. Quisque sem dui, commodo a dui consectetur, posuere accumsan orci. Maecenas luctus, nibh ac molestie ornare, mi odio placerat metus, vitae ornare lorem lectus a erat. Maecenas imperdiet, dui nec tincidunt sollicitudin, orci velit efficitur tortor, id interdum odio nibh nec sapien.
-
-// Utilize laços de repetição e estrutura de dados `Map` pra descobrir quanto cada palavra foi repetida e depois verificar qual delas foi mais repetidas.
-
-let texto =
-  "Ut interdum tellus sit amet arcu viverra facilisis. Mauris diam magna, condimentum ac justo nec, rhoncus iaculis odio. Sed tincidunt lacus ut risus laoreet sagittis. Curabitur tristique egestas libero, nec vulputate ligula pellentesque auctor. Mauris vitae est lacinia, condimentum felis eget, consequat eros. Proin tempor ligula felis, sit amet malesuada purus viverra sit amet. Nunc lobortis porta est a mattis.";
-
-let palavras = texto.replace(/[.,]/g, "").toLowerCase().split(/\s+/);
+let palavras = texto.toLowerCase().replace(/[.,]/g, "").split(" ");
 let contador = new Map();
 
 for (let palavra of palavras) {
@@ -25,15 +16,15 @@ for (let palavra of palavras) {
 }
 
 let palavraMaisRepetida = "";
-let maxContagem = 0;
+let maiorContador = 0;
 
 for (let [palavra, contagem] of contador) {
-  if (contagem > maxContagem) {
-    maxContagem = contagem;
+  if (contagem > maiorContador) {
     palavraMaisRepetida = palavra;
+    maiorContador = contagem;
   }
 }
 
 console.log(
-  `A palavra mais repetida é '${palavraMaisRepetida}' com ${maxContagem} ocorrências.`
+  `A palavra que mais se repete é "${palavraMaisRepetida}" e aparece ${maiorContador} vezes.`
 );
